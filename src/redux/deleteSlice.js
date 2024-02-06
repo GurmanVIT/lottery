b7f4c9730ec247c6fa64ed69a8754ace4789269f
AnmolDang <anmoldang121@gmail.com>
2024-02-06 21:52:36 +0530

    'notification'

--- src/redux/deleteSlice.js
+++ src/redux/deleteSlice.js
@@ -3,52 +3,54 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { ApiBaseUrl, deleteApi } from "../utils/Constants";
 
-export const deleteNotifications = createAsyncThunk("deleteNotifications", async (payload) => {
+export const deleteNotifications = createAsyncThunk(
+  "deleteNotifications",
+  async (payload) => {
     try {
-        const token = localStorage.getItem("token");
-        const config = {
-            headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Content-Type": "application/json",
-                authorization: token,
-            },
-        };
-        const url =
-            ApiBaseUrl +
-            deleteApi
-        const response = await axios.put(url, config);
-        return response.data;
+      const token = localStorage.getItem("token");
+      console.log(token);
+      const config = {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+          authorization: token,
+        },
+      };
+      const url = ApiBaseUrl + deleteApi;
+      const response = await axios.put(url, payload, config);
+      return response.data;
     } catch (error) {
-        throw error.response.data;
+      throw error.response.data;
     }
-});
+  }
+);
 
 const deleteSlice = createSlice({
-    name: "deleteReducer",
+  name: "deleteReducer",
 
-    initialState: {
-        isLoading: false,
-        data: null,
+  initialState: {
+    isLoading: false,
+    data: null,
+  },
+  reducers: {
+    clearData: (state) => {
+      // Reset the data property to an empty array
+      state.data = null;
     },
-    reducers: {
-        clearData: (state) => {
-            // Reset the data property to an empty array
-            state.data = null;
-        },
-    },
-    extraReducers: (builder) => {
-        builder
-            .addCase(deleteNotifications.pending, (state) => {
-                state.isLoading = true;
-            })
-            .addCase(deleteNotifications.fulfilled, (state, action) => {
-                state.isLoading = false;
-                state.data = action.payload;
-            })
-            .addCase(deleteNotifications.rejected, (state, action) => {
-                state.isError = false;
-            });
-    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(deleteNotifications.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(deleteNotifications.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.data = action.payload;
+      })
+      .addCase(deleteNotifications.rejected, (state, action) => {
+        state.isError = false;
+      });
+  },
 });
 
 export const { clearData } = deleteSlice.actions;
